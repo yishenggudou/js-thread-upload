0394de4cb6d051a6ec617fe0eb2fe8778a094cab
timger <yishenggudou@gmail.com>
2012-09-24 15:39:03 +0800

    修改uploadjs

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,47 +1,22 @@
-$(document).ready(function(){
-     //var a='<input name=\"test\" type=file   id="qsstest"/>';
-     //$('body').prepend(a);
-     //https://developer.mozilla.org/en-US/docs/JavaScript_typed_arrays/ArrayBuffer
-     //http://robertnyman.com/2010/03/25/using-html5-web-workers-to-have-background-computational-power/
-    var workers = [1,2,3,4];
-    workers = _.map(workers,function(num){
-                                return new Worker('/static/js/worker.js')
-                                    }
-                )
-     $('#filesubmit').click(function(){
-         var fileobj = document.getElementById('qsstest').files[0];
-         //reader.readAsArrayBuffer(fileobj);
-         //reader.onload = function(e){
-         var URL = 'http://qss.qiyi.domain/upx',list = [],worker_tasks=[];
-         $.ajax({
-                type:"POST",
-                url:URL,
-                data:{file_size:fileobj.size},
-                dataType:'json',
-                crossDomain: true,
-                success:function(data){
-                    console.log(["revice task from serer",data]);
-                    var url="http://qss.qiyi.domain/upu";
-                    console.log(["worker pools",workers]);
-                    for  (task_id in data.bitmap){
-                        var worker = workers[Math.floor(Math.random()*workers.length)];
-                        task  = data.bitmap[task_id];
-                        data.task_id = task_id;
-                        var buff = fileobj.slice(task[1],task[2]);
-                        var work = {worker_index:task_id,buff:buff,url:url,task:task,upx:data};
-                        //console.log(work,buff);
-                        worker.postMessage(work)
-                        //console.log(['revice a buff from worker',buff,url]);
-                        worker.onmessage = function(e){
-                            worker_tasks.push(e.data);
-                            if (!e.data.status){
-                                console.log(["has hadle a worker",e])
-                                //worker.postMessage([])
-                            }                         
-                        }
-                    }
-                }
-            });
-         return false;
-     })
-})
+(
+    setting = {qss:{
+                    host:'http://api.qss.inter.iqiyi.com/',
+                    bucket:'webpan',
+                    access_id:'SngbmhdimpvnhAzhemhh',
+                    access_key:'6ccd1269d00be1d1b799b86c54eab245',
+                    upxuri:'http://api.qss.inter.iqiyi.com/upx',
+                    upuuri:"http://api.qss.inter.iqiyi.com/upu"
+                    },
+                elem_id:'',
+                upload:{
+                        before:function(){},
+                        mode:'loop',   //loop thread
+                        after:function(){},
+                        process:function(){},
+                        finish:function(){},
+                        } 
+                };
+    
+
+
+)(window)
--- static/js/worker.js
+++ static/js/worker.js
@@ -1,44 +1,6 @@
-/*worker.js use for thread task
- * https://developer.mozilla.org/en-US/docs/DOM/Using_web_workers?redirectlocale=en-US&redirectslug=Using_web_workers
- * https://developer.mozilla.org/en-US/docs/DOM/Worker
- */
-/*
-POST /cgi-bin/upload.pl HTTP/1.1
-Accept: application/vnd.ms-excel, application/msword, application/vnd.
-+ms-powerpoint, image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, a
-+pplication/pdf, 
-Referer: http://deville/cgi-bin/upload.pl
-Accept-Language: en-us
-Content-Type: multipart/form-data; boundary=--------------------------
-+-7d03135102b8
-Accept-Encoding: gzip, deflate
-User-Agent: Mozilla/4.0 (compatible; MSIE 5.01; Windows NT)
-Host: deville
-Content-Length: 316
-Connection: Keep-Alive
-
------------------------------7d03135102b8
-Content-Disposition: form-data; name="file"; filename="D:\hess\dev\www
-+\notes\hello.txt"
-Content-Type: text/plain
-
-Hello there
-
------------------------------7d03135102b8
-Content-Disposition: form-data; name="done"
-
-done
------------------------------7d03135102b8--
-*/
-
-
 /*
 * worker available https://developer.mozilla.org/en-US/docs/DOM/Worker/Functions_available_to_workers
 */
-
-
-
-
 //https://developer.mozilla.org/en-US/docs/DOM/Using_web_workers?redirectlocale=en-US&redirectslug=Using_web_workers
 //importScripts("/static/js/formdata.js)
 importScripts("/static/js/xhr-formdata.js")
